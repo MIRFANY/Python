81d41fac0b55f36ffa36b39a26708978aef21934
Irfan <mohdirfan68883@gmail.com>
2025-08-23 00:02:55 +0530

    Add files via upload

--- first.py
+++ first.py
@@ -1,1 +1,59 @@
-print('Hello world')
+urString = "THis is a python  lab"
+# print(urString [:: -1])
+
+# print (urString[0])
+
+print(urString[6:0])
+
+
+
+print(urString[::])
+
+print(urString[:])
+
+
+a=2
+b=5
+
+
+print(urString.upper())
+print(urString.lower())
+print(urString.split())
+print(urString.count('i'))
+print(urString.capitalize())
+print(urString.casefold())
+
+
+# urString[0]='I' as it is immutable string 
+
+print('hello {}'.format('hello world'))
+
+print(f"The sum of {a} and {b} is {a+b}")
+
+
+
+c=5
+d=9
+print(f"The multiplication of {a} and {b} is {a*b}")
+
+num1 = 34.12
+num2 = 12.11
+
+print(f"The multiplication of {num1} and {num2} is {num1 * num2}")
+
+
+x=10
+y =11
+z=x/y
+print("The value of z: {}".format(z))
+
+print("The value of z: {:.2f}".format(z))
+
+
+
+
+
+
+
+
+
